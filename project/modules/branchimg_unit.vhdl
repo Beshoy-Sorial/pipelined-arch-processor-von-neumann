168c0f156769ff0871a82800ddf8a3232e652b42
mohamed-yasser23 <147346799+mohamed-yasser23@users.noreply.github.com>
2025-12-19 22:07:52 +0200

    Refactor branching unit by removing flush_branch_branch
    Removed flush_branch_branch output and related assignments.

--- project/modules/branchimg_unit.vhdl
+++ project/modules/branchimg_unit.vhdl
@@ -10,7 +10,6 @@ entity branching_unit is
 
         branch_sel : out std_logic;
         flush_decode_execute_branch : out std_logic;
-        flush_branch_branch : out std_logic
     );
 end branching_unit;
 
@@ -20,25 +19,18 @@ begin
     begin
         -- default outputs (VERY IMPORTANT)
         branch_sel <= '0';
-        flush_branch_branch <= '0';
         flush_decode_execute_branch <= '0';
 
         if en = '1' then
-            -- unconditional branch
-            if branch_type = "011" then
-                branch_sel <= '1';
-                flush_branch_branch <= '1';
-                flush_decode_execute_branch <= '1';
-
             -- conditional branches
-            elsif (branch_type = "000" and flags(0) = '1') or  -- ZERO
+            if (branch_type = "000" and flags(0) = '1') or  -- ZERO
                   (branch_type = "001" and flags(1) = '1') or  -- NEG
                   (branch_type = "010" and flags(2) = '1') then -- CARRY
 
                 branch_sel <= '1';
-                flush_branch_branch <= '1';
                 flush_decode_execute_branch <= '1';
-            end if;
+         
+        end if;
         end if;
     end process;
 end rtl;
