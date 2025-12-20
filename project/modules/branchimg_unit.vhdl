6e3924ae8f1ef01eed50e67e0440254a67d07887
mohamed-yasser23 <147346799+mohamed-yasser23@users.noreply.github.com>
2025-12-20 12:28:39 +0200

    Fix formatting and indentation in branchimg_unit.vhdl

--- project/modules/branchimg_unit.vhdl
+++ project/modules/branchimg_unit.vhdl
@@ -9,7 +9,7 @@ entity branching_unit is
         branch_type : in std_logic_vector(2 downto 0);
 
         branch_sel : out std_logic;
-        flush_decode_execute_branch : out std_logic;
+        flush_decode_execute_branch : out std_logic
     );
 end branching_unit;
 
@@ -24,13 +24,13 @@ begin
         if en = '1' then
             -- conditional branches
             if (branch_type = "000" and flags(0) = '1') or  -- ZERO
-                  (branch_type = "001" and flags(1) = '1') or  -- NEG
-                  (branch_type = "010" and flags(2) = '1') then -- CARRY
+               (branch_type = "001" and flags(1) = '1') or  -- NEG
+               (branch_type = "010" and flags(2) = '1') then -- CARRY
 
                 branch_sel <= '1';
                 flush_decode_execute_branch <= '1';
-         
-        end if;
+            end if;
         end if;
     end process;
 end rtl;
+
